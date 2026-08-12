067a1c601915c80f73131e0be0b6193bc039d647
Jean Bruenn <himself@jeanbruenn.info>
2026-08-12 21:00:13 +0200

    test: update bootstrap tests for showcase controller

--- tests/BootstrapTest.php
+++ tests/BootstrapTest.php
@@ -4,8 +4,13 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use Components\HelloWorld\Controllers\HelloController;
 use PHPUnit\Framework\TestCase;
 use Safi\Core\Assembler;
+use Safi\Core\Contracts\DatabaseDriverInterface;
+use Safi\Core\Contracts\SecurityServiceInterface;
+use Safi\Core\Contracts\ViewEngineInterface;
+use Safi\Core\Http\Request;
 use Safi\Core\Kernel;
 
 final class BootstrapTest extends TestCase
@@ -20,5 +25,24 @@ final class BootstrapTest extends TestCase
 
         $this->assertInstanceOf(Assembler::class, $boot['assembler']);
         $this->assertInstanceOf(Kernel::class, $boot['kernel']);
+    }
+
+    public function testHelloWorldControllerReturnsResponse(): void
+    {
+        $view = $this->createMock(ViewEngineInterface::class);
+        $view->expects($this->once())
+            ->method('render')
+            ->with('@HelloWorld/example', $this->isType('array'))
+            ->willReturn('<h1>Showcase</h1>');
+
+        $request = new Request([], [], ['REQUEST_METHOD' => 'GET', 'REQUEST_URI' => '/hello']);
+        $security = $this->createMock(SecurityServiceInterface::class);
+        $db = $this->createMock(DatabaseDriverInterface::class);
+
+        $controller = new HelloController($view, $request, $security, $db);
+        $response = $controller->index();
+
+        $this->assertSame(200, $response->getStatus());
+        $this->assertSame('<h1>Showcase</h1>', $response->getContent());
     }
 }
